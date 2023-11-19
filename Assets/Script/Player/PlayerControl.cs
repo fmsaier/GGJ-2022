b45c1fc92f46a4e81202630b2d13b3d1ec0c55bb
fmsaier <2661458618@qq.com>
2023-11-19 22:16:56 +0800

    做了个小人物，构思地图中

---
+++ Assets/Script/Player/PlayerControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 4.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector2 position = transform.position;
+        position.x += h * speed *Time.deltaTime;
+        transform.position = position;
+    }
+}
